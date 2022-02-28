6ffa22963e540d9c6ceb479490cd041a9d63b291
Elpistolero13 <69346261+Elpistolero131@users.noreply.github.com>
2022-02-28 17:48:06 +0100

    Add files via upload

---
+++ bin/Help-Util/help.temp.js
@@ -0,0 +1,50 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+const colors = require('../../../Storage/json/colors.json')
+
+module.exports = {
+    name: 'help-temp',
+    aliases: ["h-t", "h-we", "help-weather"],
+
+    onlyUsers: ["509765051435974692", "691644619758370846"],
+    run: async(client, message, args, container) => {
+
+        var prefix = db.get(`prefix_${message.guild.id}` || container.Config.prefix)
+        let lang = client.langs.get(db.get(`lang_${message.guild.id}`) || 'en');
+
+        message.reply({
+            embeds: [
+            new container.Discord.MessageEmbed()
+            .setColor(colors.PERSO)
+            .setTitle(lang.commands.help.helpT[1].replace('{PREFIX}', prefix))
+            .setDescription(lang.commands.helpa[4])
+            .addFields({
+                name: lang.commands.helpa[5],
+                value: lang.commands.help.helpT[2]
+            }, {
+                name: lang.commands.helpa[6],
+                value: lang.commands.help.helpT[3].replace('{PREFIX}', prefix),
+                inline: true
+            }, {
+                name: lang.commands.helpa[7],
+                value: `\`${prefix}temp\`\n\`${prefix}t\``,
+                inline: true
+            }, {
+                name: lang.commands.helpa[2],
+                value: lang.commands.helpa[1]
+            }, {
+                name: lang.commands.helpa[9],
+                value: "`Util`"
+            }, {
+                name: "Cooldown",
+                value: "0s"
+            }, {
+                name: lang.commands.helpa[10],
+                value: lang.commands.helpa[11]+`\n\n[${lang.commandsa[0]}](https://nepust.fr/)`
+            })
+             .setFooter({text: `© ${client.user.username}`,  iconURL: client.user.avatarURL()})
+            .setTimestamp()
+        ]
+    })
+    }
+}
