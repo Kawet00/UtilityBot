6312c1815b0dbd40683969da06f1bc9a77c1c87c
Elpistolero13 <69346261+Elpistolero131@users.noreply.github.com>
2022-03-08 12:12:45 +0100

    Update Config.js

--- Root/Storage/Vault/Config.js
+++ Root/Storage/Vault/Config.js
@@ -1,7 +1,7 @@
 module.exports = {
     prefix: ["*"],
     developers: ["691644619758370846"],
-    token: "Nzg3MzA2MTU4MzAxNTExNjkx.X9TB_w.flWirLnb6wmtU6aMS7PnPBL7B3E",
+    token: "Nzg3MzA2MTU4MzAxNTExNjkx.X9TB_w.KL2GpOafcH8IJFLe-sQfZFFi5Vg",
     AME_API:"bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d",
     supporGuild: "759700194820096011",
     reportChannel: "799932188442886176",
