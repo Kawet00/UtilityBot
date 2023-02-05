14f877b6390c78afe5e3bef4864f33eb4fba4e62
Elpistolero13 <69346261+Elpistolero131@users.noreply.github.com>
2023-02-05 11:23:18 +0100

    Delete Root directory

--- Root/Events/GuildDelete.js
+++
@@ -1,9 +0,0 @@
-const db = require('../../UtilityBotFinal/Root/Storage/db/manager')
-
-module.exports = {
-    name: "guildDelete",
-
-    run: async (guild, client) => {
-        await db.deleteGuild(guild.id);
-    }
-}
--- Root/Website/backend/CheckAuth.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (req, res, next) => {
-    if (req.isAuthenticated()) return next();
-    req.session.backURL = req.url;
-    res.redirect("/login");
-}
